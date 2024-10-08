87ab6d11df6ecf4803190a8de6747714bc36d5de
Hani <hhoumimid@gmail.com>
2024-10-08 10:24:37 +0200

    Le test réussi

--- src/test/java/r5a08_findmyword/test_Word.java
+++ src/test/java/r5a08_findmyword/test_Word.java
@@ -10,7 +10,7 @@ public class test_Word {
         Word word = new Word("E"); // Le mot à déviner fait une lettre
         // Act
         String expected = "E";
-        String actual = word.guess("B");
+        String actual = word.guess("E");
         // Assert
         Assertions.assertEquals(expected, actual);
     }
